bc834910532375dd2ef238f0af896fe15258912f
Nicholas Oram <nickoram@gmail.com>
2016-01-27 15:25:00 -0400

    introduced controller for store
    - encapsulated js within app.js

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-var app = angular.module('store', []);
+(function() {
+  var app = angular.module('store', []);
+
+  var gem = {
+      name: 'Dodecahedron',
+      price: 2.99,
+      description: 'Mea sale noluisse ex, aeque theophrastus in qui.'
+  }
+
+  app.controller('StoreController', function() {
+    this.product = gem;
+  });
+
+})();
