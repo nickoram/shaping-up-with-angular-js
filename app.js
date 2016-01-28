66568916df715e122fe0927bbbb3bf2723eb1fae
Nicholas Oram <nickoram@gmail.com>
2016-01-28 13:48:32 -0400

    introduce ng-src directive for img tag
    - also pop in orderBy filter with options for price

--- app.js
+++ app.js
@@ -1,25 +1,37 @@
 (function() {
   var app = angular.module('store', []);
 
+  app.controller('StoreController', function() {
+    this.products = gems;
+  });
+
   var gems = [
       {
           name: 'Dodecahedron',
           price: 2,
           description: 'Mea sale noluisse ex, aeque theophrastus in qui.',
           canPurchase: false,
-          soldOut: true
+          soldOut: true,
+          images: [
+              {
+                  full: 'images/hexagem-full.png',
+                  thumb: 'images/hexagem-thumb.png'
+              }
+          ]
       },
       {
           name: 'Pentagonal Gem',
           price: 5.99,
           description: 'Duis at tincidunt sem. Praesent ultricies sed sodales.',
           canPurchase: true,
-          soldOut: false
+          soldOut: false,
+          images: [
+              {
+                  full: 'images/octogem-full.png',
+                  thumb: 'images/octogem-thumb.png'
+              }
+          ]
       }
   ];
-
-  app.controller('StoreController', function() {
-    this.products = gems;
-  });
 
 })();
