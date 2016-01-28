d42d30e831dbcc218a0e8d78d4738eab76973d8e
Nicholas Oram <nickoram@gmail.com>
2016-01-28 10:55:45 -0400

    introduce ng-repeat directive
    - takes an expression

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 (function() {
   var app = angular.module('store', []);
 
-  var gem = {
-      name: 'Dodecahedron',
-      price: 2.99,
-      description: 'Mea sale noluisse ex, aeque theophrastus in qui.',
-      canPurchase: false,
-      soldOut: false
-  }
+  var gems = [
+      {
+          name: 'Dodecahedron',
+          price: 2.99,
+          description: 'Mea sale noluisse ex, aeque theophrastus in qui.',
+          canPurchase: false,
+          soldOut: true
+      },
+      {
+          name: 'Pentagonal Gem',
+          price: 5.99,
+          description: 'Duis at tincidunt sem. Praesent ultricies sed sodales.',
+          canPurchase: true,
+          soldOut: false
+      }
+  ];
 
   app.controller('StoreController', function() {
-    this.product = gem;
+    this.products = gems;
   });
 
 })();
