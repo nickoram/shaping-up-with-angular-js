d697bccf3ade976eaf4a1b0a32491a51d2b7ff12
Nicholas Oram <nickoram@gmail.com>
2016-01-28 15:30:22 -0400

    add PanelController to init and config panels on click
    - init and config logic should be place in controller
    - dirty code in html

--- app.js
+++ app.js
@@ -2,7 +2,19 @@
   var app = angular.module('store', []);
 
   app.controller('StoreController', function() {
-    this.products = gems;
+      this.products = gems;
+  });
+
+  app.controller('PanelController', function() {
+      this.tab = 1;
+
+      this.selectTab = function(setTab) {
+          this.tab = setTab;
+      };
+
+      this.isSelected = function(checkTab) {
+          return this.tab === checkTab;
+      };
   });
 
   var gems = [
