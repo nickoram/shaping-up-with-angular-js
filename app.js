b334e810a2b72573da3291959d146bea36950cc7
Nicholas Oram <nickoram@gmail.com>
2016-02-02 14:48:55 -0400

    use $http service to retrieve JSON data for products ...
    - services give your controller additional functionality like
     - fetching JSON data from a web service with $http
      - $http( { method: 'GET', url: '/products.json' });
     - logging messages to the javascript console with $log
     - filtering an array with $filter
    
    - dependency injection
     - app.controller('SomeController', [ '$http', function($http) {
         ...
       }]);
     - app.controller('SomeController', [ '$http', '$log', function($http, $log) {
          ...
       }]);

--- app.js
+++ app.js
@@ -1,44 +1,13 @@
 (function() {
   var app = angular.module('store', ['store-products']);
 
-  app.controller('StoreController', function() {
-      this.products = gems;
-  });
+  app.controller('StoreController', ['$http', function($http) {
+      var store = this;
+      store.products = [];
 
-  var gems = [
-      {
-          name: 'Dodecahedron',
-          price: 2,
-          description: 'Mea sale noluisse ex, aeque theophrastus in qui.',
-          canPurchase: false,
-          soldOut: true,
-          images: [
-              'images/hexagem-1.png',
-              'images/hexagem-2.png',
-              'images/hexagem-3.png'
-          ],
-          reviews: [
-              {
-                  stars: 5,
-                  body: "This gem rocks!",
-                  author: "hschrader@nm.dea.gov",
-                  createdOn: 1397490980837
-              }
-          ]
-      },
-      {
-          name: 'Pentagonal Gem',
-          price: 5.99,
-          description: 'Duis at tincidunt sem. Praesent ultricies sed sodales.',
-          canPurchase: true,
-          soldOut: false,
-          images: [
-              'images/octogem-1.png',
-              'images/octogem-2.png',
-              'images/octogem-3.png'
-          ],
-          reviews: []
-      }
-  ];
+      $http.get('/store-products.json').success(function(data) {
+          store.products = data;
+      });
+  }]);
 
 })();
